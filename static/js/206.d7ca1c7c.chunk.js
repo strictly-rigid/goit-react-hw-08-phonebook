eb65ac2c7c651f862093ecf7904a7af37f62edc7
strictly-rigid <121813691+strictly-rigid@users.noreply.github.com>
2023-09-04 16:11:43 +0000

    Deploying to gh-pages from @ strictly-rigid/goit-react-hw-08-phonebook@e72c5cb65307b2bd82b5aef95af1962489252343 🚀

--- static/js/206.d7ca1c7c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{206:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function l(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"This will be a fancy Homepage"})})}}}]);
-//# sourceMappingURL=206.d7ca1c7c.chunk.js.map
